945ac952c2ca5c62463c9a40f93a1b8f83fac45e
Vincent Meijer <meijer.vincent@gmail.com>
2019-09-05 12:19:35 +0200

    #4 Add CameraSensorTypeDictionary

---
+++ DOFCalculator/Client/Models/CameraSensorTypeDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnitsNet;
+
+namespace DOFCalculator.Client.Models
+{
+    public class CameraSensorTypeDictionary : Dictionary<string, UnitsNet.Length>
+    {
+        public static CameraSensorTypeDictionary Default => new CameraSensorTypeDictionary
+        {
+            { "Full-Frame Camera",                  Length.FromMillimeters(0.030) },
+            { "APS-C 1.6x Crop (Canon)",            Length.FromMillimeters(0.019) },
+            { "APS-C 1.5x Crop (Nikon, Sony NEX)",  Length.FromMillimeters(0.020) },
+            { "Micro 4/3 (Olympus, Panasonic)",     Length.FromMillimeters(0.015) }
+        };
+    }
+}
