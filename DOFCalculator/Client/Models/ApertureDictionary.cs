ea1c444639f6dbb3092e0c791eb18b0f63330218
Vincent Meijer <meijer.vincent@gmail.com>
2019-09-05 11:30:03 +0200

    #5 Add dictionary for Aperture

---
+++ DOFCalculator/Client/Models/ApertureDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace DOFCalculator.Client.Models
+{
+    public class ApertureDictionary : Dictionary<string, double>
+    {
+        public ApertureDictionary Default()
+        {
+            return new ApertureDictionary
+            {
+                {"f/1",     1},
+                {"f/1.2",   1.189207115},
+                {"f/1.4",   1.414213562},
+                {"f/1.8",   1.781797436},
+                {"f/2",     2.0},
+                {"f/2.8",   2.8},
+                {"f/4",     4.0},
+                {"f/5.6",   5.7},
+                {"f/8",     8.0000000},
+                {"f/11",    11.313708},
+                {"f/16",    16.0},
+                {"f/22",    22.6},
+                {"f/32",    32.0}
+            };
+        }
+    }
+}
