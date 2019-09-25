314fefa089e2d1dd32f25c4a9295e6fed601dd3d
Vincent Meijer <meijer.vincent@gmail.com>
2019-09-25 15:22:04 +0200

    #3 Add UnitMultiplicationFactor dictionary type

---
+++ DOFCalculator/Client/Models/UnitMultiplicationFactors.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnitsNet.Units;
+
+namespace DOFCalculator.Client.Models
+{
+    public class UnitMultiplicationFactors : Dictionary<LengthUnit, double>
+    {
+        public static UnitMultiplicationFactors Default => new UnitMultiplicationFactors
+        {
+            { LengthUnit.Meter, 1 },
+            { LengthUnit.Foot, 0.3048 }
+        };
+    }
+}
