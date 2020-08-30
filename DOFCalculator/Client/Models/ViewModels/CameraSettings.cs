9f3fecf46be4cecc6bfead0362d6c8124aa3352b
Vincent Meijer <meijer.vincent@gmail.com>
2020-08-30 13:55:13 +0200

    Add CameraSettings

---
+++ DOFCalculator/Client/Models/ViewModels/CameraSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DOFCalculator.Client.Models.ViewModels
+{
+    public class CameraSettings
+    {
+        public Aperture Aperture { get; }
+        public CameraSensorType CameraSensorType { get; }
+        public UnitsNet.Length LensForcalLength { get; }
+        public UnitsNet.Length SubjectDistance { get; }
+        public UnitsNet.Units.LengthUnit SubjectDistanceUnit { get; }
+    }
+}
